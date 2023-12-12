2caa0fe13c62048d98b47487657f0aa5e7eab648
mrodriguez <manuelrodriguezmog@gmail.com>
2023-12-12 15:29:35 -0400

    Feat: Agrega conexion pagina especifica con servidor

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -1,22 +1,31 @@
 import AppNavbar from "@/components/ui/Navbar";
 import Footer from "@/components/ui/Footer";
+import Image from "next/image";
 
-export const getProduct = async (context) => {
-  /*   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_ENDPOINT}/inventory/${context?.params?.id}`
+export const getProduct = async (id) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_ENDPOINT}/inventory/${id}`
   );
   const product = await res.json();
 
-  return product; */
+  return product;
 };
 
-const product = getProduct();
+export default async function Product({ params }) {
+  const { id } = params;
+  const product = await getProduct(id);
 
-export default function Product() {
   return (
     <>
       <AppNavbar />
-      <h3>Single Item</h3>
+      <h3>{product?.brand + " " + product?.model + " " + product?.version}</h3>
+      <Image
+        alt="latest-1"
+        className="mt-2 h-full"
+        src={product?.images[0]}
+        width={231}
+        height={132}
+      />
       <Footer />
     </>
   );
