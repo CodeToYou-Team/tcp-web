c51da7a86737fd64491f7985e5920e45009df055
mrodriguez <manuelrodriguezmog@gmail.com>
2024-01-10 20:43:27 -0400

    Fix: utiliza solo componente VehicleCard en layouts

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -3,11 +3,14 @@ import Footer from "@/components/ui/Footer";
 import ImageSlider from "@/components/ui/ImageSlider";
 import { getVehicleById } from "@/lib/services";
 import VehicleInfo from "../layouts/VehicleInfo";
-import RecommendedVehicles from "../layouts/RecommendedVehicles";
+import VehicleCard from "@/components/VehicleCard";
+import { getRecommendationCars } from "@/lib/services";
 
 export default async function Product({ params }) {
   const { id } = params;
   const vehicle = await getVehicleById(id);
+
+  const recommendedVehicles = await getRecommendationCars(id, vehicle.brand);
 
   return (
     <>
@@ -17,7 +20,19 @@ export default async function Product({ params }) {
         <ImageSlider vehicle={vehicle} />
         <VehicleInfo vehicle={vehicle} />
       </div>
-      <RecommendedVehicles id={id} brand={vehicle.brand} />
+
+      <div className="py-12 bg-zinc-900">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl">
+            <h2 className="text-center text-3xl font-semibold leading-8">
+              Otro vehiculos que te pueden interesar
+            </h2>
+          </div>
+          <div className="w-full mt-24 gap-2 grid grid-cols-12 px-8">
+            <VehicleCard vehicles={recommendedVehicles} />
+          </div>
+        </div>
+      </div>
 
       <Footer />
     </>
--- app/page.js
+++ app/page.js
@@ -2,11 +2,13 @@ import Navbar from "@/components/ui/Navbar";
 import Hero from "./layouts/Hero";
 import Footer from "@/components/ui/Footer";
 import Brands from "./layouts/Brands";
-import LatestCars from "./layouts/LatestCars";
 import Steps from "./layouts/Steps";
 import { Suspense } from "react";
+import VehicleCard from "@/components/VehicleCard";
+import { getLatestVehicles } from "@/lib/services";
 
-export default function Home() {
+export default async function Home() {
+  const latestVehicles = await getLatestVehicles();
   return (
     <>
       <Navbar />
@@ -14,7 +16,18 @@ export default function Home() {
       <Brands />
       <Steps />
       <Suspense fallback={<div>Loading...</div>}>
-        <LatestCars />
+        <div className="py-12 bg-zinc-900">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl">
+              <h2 className="text-center text-3xl font-semibold leading-8">
+                ¡Échale un vistazo a nuestras últimas incorporaciones!
+              </h2>
+            </div>
+            <div className="w-full mt-24 gap-2 grid grid-cols-12 px-8">
+              <VehicleCard vehicles={latestVehicles} />
+            </div>
+          </div>
+        </div>
       </Suspense>
       <Footer />
     </>
