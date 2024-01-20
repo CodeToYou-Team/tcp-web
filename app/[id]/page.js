c5c95c8fad7571df97f7d150bc9edb8b0822f8ab
Miguelangel <mestevezo23@gmail.com>
2024-01-19 22:02:51 -0400

    ⚗️ feat: probando metatags en ruta dinámica

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -7,6 +7,7 @@ import VehicleCard from "@/components/VehicleCard";
 import { getRecommendationCars } from "@/lib/services";
 import { VehiclesLayout } from "../layouts/VehiclesLayout";
 import VehicleDetails from "../layouts/VehicleDetails";
+import Head from "next/head";
 
 export default async function Product({ params }) {
   const { id } = params;
@@ -16,6 +17,17 @@ export default async function Product({ params }) {
 
   return (
     <>
+      <Head>
+        <title>{`${vehicle.brand} ${vehicle.model} - Tu Carro Propio`}</title>
+        <meta
+          name="title"
+          content={`${vehicle.brand} ${vehicle.model} - Tu Carro Propio`}
+        />
+        <meta
+          name="description"
+          content={`Descubre este increíble ${vehicle.brand} ${vehicle.model}. ¡Programa una cita para conocer más detalles!`}
+        />
+      </Head>
       <Navbar />
       {/* Imagen y cabecera del vehículo */}
       <div className="mx-auto max-w-7xl px-6 lg:px-8 mt-4 grid grid-cols-1 md:grid-cols-2 gap-6">
