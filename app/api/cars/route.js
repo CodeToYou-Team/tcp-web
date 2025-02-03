b5d7c2f620f6c0ee195c0ccfbb8c9fe7fe1aae4d
Jesus Velasco <jesusvelascov@gmail.com>
2025-02-03 19:34:31 -0400

    Feat: Agrega endpoint para consultar carros

---
+++ app/api/cars/route.js
@@ -0,0 +1,22 @@
+"use server";
+
+import connectMongo from "@/app/lib/connect-mongo";
+import InventoryDB from "@/app/lib/models/Inventory.js";
+import { NextResponse } from 'next/server';
+
+export async function GET(req, res) {
+
+  if (req.method === 'GET') {
+
+    await connectMongo();
+
+    const items = await InventoryDB.find({ enabled: true });
+
+    return NextResponse.json({ cars: JSON.parse(JSON.stringify(items)) }, { status: 200 });
+
+  } else {
+    
+    return NextResponse.json({ message: 'Invalid method!' }, { status: 400 });
+
+  }
+}
