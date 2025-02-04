f4e965f16ba57dfec7b2877bb565d26aebbed69c
Miguelangel <mestevezo23@gmail.com>
2025-02-03 21:17:04 -0400

    🚀 feat: deploy de api

--- app/api/cars/route.js
+++ app/api/cars/route.js
@@ -2,21 +2,19 @@
 
 import connectMongo from "@/app/lib/connect-mongo";
 import InventoryDB from "@/app/lib/models/Inventory.js";
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 export async function GET(req, res) {
-
-  if (req.method === 'GET') {
-
+  if (req.method === "GET") {
     await connectMongo();
 
     const items = await InventoryDB.find({ enabled: true });
 
-    return NextResponse.json({ cars: JSON.parse(JSON.stringify(items)) }, { status: 200 });
-
+    return NextResponse.json(
+      { cars: JSON.parse(JSON.stringify(items)) },
+      { status: 200 }
+    );
   } else {
-    
-    return NextResponse.json({ message: 'Invalid method!' }, { status: 400 });
-
+    return NextResponse.json({ message: "Invalid method!" }, { status: 400 });
   }
 }
